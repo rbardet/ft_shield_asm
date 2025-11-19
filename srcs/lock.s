f6f448e3cca28ca7ebc6c60bc5867c8e1b8a3e47
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-19 12:57:35 +0100

    Feat: add label for readability

--- srcs/lock.s
+++ srcs/lock.s
@@ -1,22 +1,20 @@
+%include "ft_shield.inc"
+
+section .bss
+    lockfd resw 2
+
 section .data
     LOCK_FILE dq "ft_shield.lock", 0
     LOCK_ERROR dq "daemon already running", 10
     LOCK_ERROR_LEN equ $ - LOCK_ERROR
-    O_CREAT equ 0100
-    LOCK_EX equ 1
-    F_OK equ 0
 
 section .text
     global create_lock
     global existing_lock
-    extern exit
-    extern print_error
-    extern SYS_OPEN
-    extern SYS_FLOCK
-    extern SYS_CLOSE
-    extern SYS_ACCESS
 
 create_lock:
+    mov word[lockfd], 0
+.open_file:
     mov rax, SYS_OPEN
     mov rdi, LOCK_FILE
     mov rsi, O_CREAT
@@ -24,24 +22,21 @@ create_lock:
     syscall
     cmp rax, 0
     jl .err_open
-    mov rdi, rax
+    mov [lockfd], rax
+.lock_file:
+    mov rdi, [lockfd]
     mov rsi, LOCK_EX
     mov rax, SYS_FLOCK
     syscall
     cmp rax, 0
     jne .err_lock
-    mov rax, SYS_CLOSE
-    syscall
-    xor rax, rax
+    close_file [lockfd]
     ret
 .err_open:
-    mov rax, 1
-    call exit
-    ret
+    exit EXIT_FAILURE
 .err_lock:
-    mov rax, 2
-    call exit
-    ret
+    close_file [lockfd]
+    exit EXIT_FAILURE
 
 existing_lock:
     mov rax, SYS_ACCESS
@@ -52,7 +47,5 @@ existing_lock:
     je .err_lock_exist
     ret
 .err_lock_exist:
-    mov rsi, LOCK_ERROR
-    mov rdx, LOCK_ERROR_LEN
-    call print_error
-    call exit
+    print STD_ERR, LOCK_ERROR, LOCK_ERROR_LEN
+    exit EXIT_FAILURE
