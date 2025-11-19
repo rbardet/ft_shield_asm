2513b7128de21fdacb1f5f13fd4194cfe62c77e6
rbardet <robin.bardet.tomczak@gmail.com>
2025-11-19 19:34:59 +0100

    Chore: got user connection just need to read input

--- srcs/run_server.s
+++ srcs/run_server.s
@@ -5,10 +5,18 @@ section .data
     TOOMANY_USER_LEN equ $ - TOOMANY_USER
     ERR_EPCTL dd "Error while connecting to server, try later", 10
     ERR_EPCTL_LEN equ $ - ERR_EPCTL
+    GOODBYE dd "Goodbye.", 10
+    GOODBYE_LEN equ $ - GOODBYE
     HERE dd "HERE", 10
     HERE_LEN equ $ - ERR_EPCTL
 section .text
     global run_server
+
+next_event:
+    mov rax, r14
+    imul rax, EPOLL_EVENT_SIZE
+    lea r13, [events + rax]
+    ret
 
 run_server:
 .server_loop:
@@ -24,15 +32,19 @@ run_server:
     mov r11, [events + epoll_event.data]
     mov rbp, rax
 .handle_event:
-    mov r12, rbx
-    mov r13, [events]
     cmp rbp, r14
     je .server_loop
-    cmp [events + epoll_event.data], r12
+    call next_event
+    cmp [r13 + epoll_event.data], rbx
     je .handle_user
+.read_input:
+    read r13, rsi, BUFFER_SIZE
+    cmp rax, 0
+    je .disconnect_user
+    jl .handle_event
+    print STD_OUT, rsi, BUFFER_SIZE
     inc r14
-    jmp .server_loop
-    ret
+    jmp .handle_event
 .handle_user:
     PUSH r12
     mov r12, [usernb]
@@ -49,10 +61,10 @@ run_server:
     syscall
     cmp rax, 0
     jl .err_accept
-    mov r14, rax
+    mov [userfd], rax
     mov dword[epoll + epoll_event.events], EPOLLIN
     mov qword[epoll + epoll_event.data], rax
-    epoll_ctl r15, EPOLL_CTL_ADD, r14
+    epoll_ctl r15, EPOLL_CTL_ADD, [userfd]
     cmp rax, 0
     jl .err_epoll
     inc byte [usernb]
@@ -65,8 +77,15 @@ run_server:
     syscall
     cmp rax, 0
     jl .err_accept
-    print rax, TOOMANY_USER, TOOMANY_USER_LEN
-    close_file rax
+    mov [userfd], rax
+    print [userfd], TOOMANY_USER, TOOMANY_USER_LEN
+    close_file [userfd]
+    jmp .handle_event
+.disconnect_user:
+    print r13, GOODBYE, GOODBYE_LEN
+    close_file r13
+    epoll_ctl r15, EPOLL_CTL_DEL, r13
+    dec byte [usernb]
     jmp .handle_event
 .err_epoll:
     print r14, ERR_EPCTL, ERR_EPCTL_LEN
